964f8a1495bd6bac9ee8e702d06af008cdafb504
Paul Nechifor <paul@nechifor.net>
2018-06-27 20:39:58 +0100

    Move to GitLab.

--- src/client-start.js
+++
@@ -1,4 +0,0 @@
-/* istanbul ignore next */
-import {Client} from './client';
-/* istanbul ignore next */
-new Client().start().el.appendTo('body');
--- test/client-setup.js
+++
@@ -1,8 +0,0 @@
-// This is a helper file which implements a lightweight server-side DOM for
-// unit-testing client-side code in Node.
-
-const jsdom = require('jsdom');
-
-const document = jsdom.jsdom('<!doctype html><html><body></body></html>', {url: 'http://localhost/'});
-global.document = document;
-global.window = document.defaultView;
